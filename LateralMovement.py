4d1ab83244eb872cf04d657d99c185a3320430e4
saidhfm <40364562+saidhfm@users.noreply.github.com>
2024-08-25 16:20:45 +0530

    Update and rename lateralMovement.py to LateralMovement.py

--- LateralMovement.py
+++ LateralMovement.py
@@ -15,7 +15,7 @@ def send_ssh_public_key(instance_id, instance_os_user, availability_zone, ssh_pu
         print(f"Failed to send SSH public key to instance {instance_id}: {e}")
 
 if __name__ == '__main__':
-    instance_id = 'i-098029b66ac7b3c47'  # Replace with your EC2 instance ID
+    instance_id = 'i-098029b66ac7b3c4'  # Replace with your EC2 instance ID
     instance_os_user = 'ubuntu'  # Replace with the appropriate OS user
     availability_zone = 'us-east-1e'  # Replace with the availability zone of your instance
     ssh_public_key_file = '/path/to/id_rsa.pub'  # Replace with the actual path to your SSH public key file
